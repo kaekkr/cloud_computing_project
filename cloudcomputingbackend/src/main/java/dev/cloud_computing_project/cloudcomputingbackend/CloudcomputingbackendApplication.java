4fe3938e9af8f100d20378d1a9b6b3d0894fb214
Karassay <karassayraushanbek@gmail.com>
2024-11-17 23:44:42 +0600

    init

---
+++ cloudcomputingbackend/src/main/java/dev/cloud_computing_project/cloudcomputingbackend/CloudcomputingbackendApplication.java
@@ -0,0 +1,13 @@
+package dev.cloud_computing_project.cloudcomputingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcomputingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcomputingbackendApplication.class, args);
+	}
+
+}
---
+++ cloudcomputingbackend/src/test/java/dev/cloud_computing_project/cloudcomputingbackend/CloudcomputingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cloud_computing_project.cloudcomputingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcomputingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
